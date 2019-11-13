b5be943f2c275020b06d434b24462fcfada5bbb1
Joshua Lemay <jspades93@gmail.com>
2019-11-13 16:48:37 -0500

    created all products route

--- server/api/products.js
+++ server/api/products.js
@@ -1,1 +1,9 @@
-//write route get all products
+const router = require('express').Router
+const Product = require('../db/models/product')
+module.exports = router
+
+router.get('/', (res, next) => {
+  Product.findAll()
+    .then(product => res.json(product))
+    .catch(next)
+})
