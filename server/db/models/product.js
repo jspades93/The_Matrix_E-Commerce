d352b6f8d754b3eba240293b8fe741920b511c60
Joshua Lemay <jspades93@gmail.com>
2019-11-13 12:07:26 -0500

    added product model

---
+++ server/db/models/product.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: true
+  }
+})
+
+module.exports = {
+  Product
+}
